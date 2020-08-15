495182b69423bb968f831c0fa41d3507e1da9e24
Vadim Zeitlin <vadim@zeitlins.org>
2020-08-15 15:29:34 +0200

    Show not only average speed, but also min/max/std deviation
    This shows that read results are very stable (σ < 1), write results are
    completely over the place, with σ > 10.

--- browser.js
+++ browser.js
@@ -47,22 +47,49 @@ window.onload = function() {
       const progress = log(`Running ${NUM_ITER} ${action} tests: `);
 
       const start = window.performance.now();
-      let totalBytes = 0;
 
+      // compute the mean and the standard deviation on the fly using the
+      // algorithm from TAoCP Vol 2, section 4.2.2.
+      let speedMin, speedMax, lastM, lastS;
 
       for (let i=0; i<NUM_ITER; ++i) {
         progress.nodeValue += '.';
 
+        const startThis = window.performance.now();
+
         const receivedData = await send(dataToSend);
+
+        const timeThis = window.performance.now() - startThis;
+
         // only one of those is non-zero
-        totalBytes += dataToSend.length + receivedData.length;
+        const bytesXfered = dataToSend.length + receivedData.length;
+
+        const speedThis = bytesXfered / 1000.0 / timeThis;
+
+        if (i == 0) {
+          speedMin =
+          speedMax = speedThis;
+
+          lastM = speedThis;
+          lastS = 0;
+        } else {
+          if (speedThis < speedMin)
+            speedMin = speedThis;
+          if (speedThis > speedMax)
+            speedMax = speedThis;
+
+          const oldM = lastM;
+          lastM += (speedThis - oldM)/(i + 1);
+          lastS += (speedThis - oldM)*(speedThis - lastM);
+        }
       }
 
       const time = window.performance.now() - start;
 
       progress.nodeValue += ' done';
 
-      log(`Elapsed: ${time}ms, average speed: ${(totalBytes / time / 1000.0).toFixed(2)}MBps`);
+      const f2 = (x) => x.toFixed(2);
+      log(`Elapsed: ${time}ms, avg speed: ${f2(lastM)}MBps, std dev ${f2(Math.sqrt(lastS/(NUM_ITER - 1)))},  min/max: ${f2(speedMin)}/${f2(speedMax)}`);
     }
   }
 
